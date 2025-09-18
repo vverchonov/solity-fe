8c6b8c5c1ab191daa6610ccceac5382670444f70
Viacheslav Verchonov <vverchonov@gmail.com>
2025-09-17 20:28:56 -0400

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Mute console.log and console.info in production
+// Only allow errors and warnings
+if (import.meta.env.NODE_ENV !== 'development') {
+  console.log = () => {};
+  console.info = () => {};
+  console.debug = () => {};
+  console.trace = () => {};
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
